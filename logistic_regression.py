f84ecd4cec38ee46a08dd61bac5878bd07191d82
Giuseppe Salvi <giuseppe.salvi98@gmail.com>
2022-01-19 12:18:17 +0100

    feat: logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,3 +1,51 @@
 """
     This module contains the implementation of the Logistic Regression model
 """
+import numpy as np
+import scipy.optimize as op
+
+
+def logreg_obj_wrap(DTR, LTR, l):
+    """ It's a wrapper for function logreg_obj that needs to access also to
+        DTR, LTR, and lambda (l)
+    """
+    def logreg_obj(v):
+        """ Computes the Logistic Regression objective J(w, b) using formula (2)
+            v is a numpy array with shape (D+1,), where D is the dimensionality of 
+            the feature space v = [w,b]
+        """
+        w, b = v[0:-1], v[-1]
+        z = 2 * LTR - 1
+        J = l / 2 * (w * w).sum() + np.log1p(np.exp(-z * (w.T.dot(DTR) + b))).mean()
+
+        # using formula 3
+        # J = l / 2 * (w * w).sum() + (LTR * np.log1p(np.exp(-w.T.dot(DTR) - b)) + (1 - LTR) * np.log1p(np.exp(w.T.dot(DTR) + b))).mean()
+
+        return J
+
+    return logreg_obj
+
+
+def logistic_regression(DTR, LTR, DTE, l):
+    """ Implementation of Logistic Regression
+        DTR, LTR are training data and labels
+        DTE are evaluation data
+        l is the hyperparameter lambda
+        returns: logistic regression score, that can be interpreted as a class posterior log-likelihood ratio 
+    """
+    # starting point
+    x0 = np.zeros(DTR.shape[0] + 1)
+
+    # Use the wrapper to pass the parameters to the function
+    logreg_obj = logreg_obj_wrap(DTR, LTR, l)
+
+    # Obtain the minimizer of J
+    x, f, d = op.fmin_l_bfgs_b(logreg_obj, x0, approx_grad=True)
+
+    # Now that the model is trained we can compute posterior
+    # log-likelihoods ratios by simply computing for each test sample xt
+    # the score s(xt) = wT xt + b
+    # compute the array of scores S
+    S = np.dot(x[0:-1].T, DTE) + x[-1]
+    
+    return S
--- main.py
+++ main.py
@@ -4,7 +4,10 @@
 
 from dataset import load_train, load_test, load_trainH, load_testH, split_db_2to1
 from gaussian_models import multivariate_gaussian_classifier2, naive_bayes_gaussian_classifier, tied_covariance_gaussian_classifier
+from logistic_regression import logistic_regression
 from measuring_predictions import minimum_detection_cost
+from numpy.lib.scimath import log
+
 
 if __name__ == "__main__":
 
@@ -31,21 +34,23 @@ if __name__ == "__main__":
     (DTR_T, LTR_T), (DTR_E, LTR_E) = split_db_2to1(DTR, LTR)
     # version with noise
     (DHTR_T, LHTR_T), (DHTR_E, LHTR_E) = split_db_2to1(DHTR, LHTR)
+
+
     print("-" * 50)
     print("SINGLE FOLD")
     print("-" * 50, "\n\n")
 
 
-    # Gaussian Models
-    print("-" * 50)
-    print("GENERATIVE MODELS")
-    print("-" * 50, "\n\n")
     for app in applications:
         pi1, Cfn, Cfp = app
         print("-" * 50)
         print("Application: pi1 = %.1f, Cfn = %d, Cfn = %d" % (pi1, Cfn, Cfp))
         print("-" * 50, "\n")
 
+        # Gaussian Models
+        #print("-" * 50)
+        #print("GENERATIVE MODELS")
+        #print("-" * 50, "\n\n")
         for name, algo in generative_models:
             DCF_min = minimum_detection_cost(algo(DTR_T, LTR_T, DTR_E), LTR_E, pi1, Cfn, Cfp)
             print("%s: minDCF = %f" %(name,DCF_min),"\n") # 0.0 for MVG and tied!!
@@ -54,7 +59,25 @@ if __name__ == "__main__":
             print("%s: minDCF = %f" %(name,DCF_min)," (noise version)\n") 
         print("")
 
-    # Logistic Regression
+        # Logistic Regression
+
+        lambda_list = [0., 1e-6, 1e-3, 1.]
+        for l in lambda_list:
+            S = logistic_regression(DTR_T, LTR_T, DTR_E, l)
+            # We can recover log-likelihood ratios by subtracting from the score s
+            # the empirical prior log-odds of the training set (slide 31)
+            llr = S - log(pi1/ (1-pi1))
+            DCF_min = minimum_detection_cost(llr, LTR_E, pi1, Cfn, Cfp)
+            print("Logistic Regression: lambda = %f, minDCF = %f" %(l,DCF_min),"\n") # 0.0 for MVG and tied!!
+
+            S = logistic_regression(DHTR_T, LHTR_T, DHTR_E, l)
+            # We can recover log-likelihood ratios by subtracting from the score s
+            # the empirical prior log-odds of the training set (slide 31)
+            llr = S - log(pi1/ (1-pi1))
+            DCF_min = minimum_detection_cost(llr, LHTR_E, pi1, Cfn, Cfp)
+            print("Logistic Regression: lambda = %f, minDCF = %f" %(l,DCF_min),"(noise version)\n") # 0.0 for MVG and tied!!
+
+
 
         
     # K-FOLD
